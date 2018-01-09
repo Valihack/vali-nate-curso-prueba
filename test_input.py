d7d91718257a81d0ceb4b2031a1bb8816958faca
Valihack <valihacking@gmail.com>
2018-01-09 21:29:49 +0100

    He cambiado el nombre de una variable

--- test_input.py
+++ test_input.py
@@ -1,10 +1,10 @@
 import random
 while (1 > 0):
 
-    random = random.randrange(10)
+    random_number = random.randrange(10)
     numero_del_usuario = int(input("Dime un numero: "))
 
-    if numero_del_usuario == random:
+    if numero_del_usuario == random_number:
         print("Has acertado el "+ str(numero_del_usuario) + " es el correcto")
-    if not numero_del_usuario == random:
+    if not numero_del_usuario == random_number:
         print("No has acertado")
