4b9bc7a394791a81c95b3e7469a3f73e0f1c00da
Valihack <valihacking@gmail.com>
2018-01-10 21:22:19 +0100

    Intento solo 5 veces

--- 1_adivinar_numero.py
+++ 1_adivinar_numero.py
@@ -1,10 +1,15 @@
+
+
+
 import random
-while (1 > 0):
+a=0
+while (5 > a):
 
     random_number = random.randrange(10)
     numero_del_usuario = int(input("Dime un numero: "))
 
     if numero_del_usuario == random_number:
-        print("Has acertado el "+ str(numero_del_usuario) + " es el correcto")
+        print("Has acertado el {} es el correcto".format(numero_del_usuario))
     if not numero_del_usuario == random_number:
         print("No has acertado")
+    a+=1
