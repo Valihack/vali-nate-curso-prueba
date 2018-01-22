7bba897cc10b9427911f6904ff2834033b6c5180
Valihack <valihacking@gmail.com>
2018-01-22 14:08:57 +0100

    Si un numero en una lista es divisible entre 3 lo remplaza con Fiz si sucede con el 5 Buzz y si sucede con los dos Bazinga

---
+++ Ejercicios/FizzBuzz_modificado.py
@@ -0,0 +1,23 @@
+cantidad_numeros=int(input("Introduce cuantos numeros quieres introducir: "))
+
+numeros_usuario=[]
+numero_del_usuario=""
+
+while len(numeros_usuario)<cantidad_numeros:
+    numero_del_usuario=int(input("Introduce numero: "))
+    numeros_usuario.append(numero_del_usuario)
+    print("Añadido!")
+
+palabra=""
+
+for contador in range(len(numeros_usuario)):
+    if numeros_usuario[contador] % 3 == 0:
+        palabra+="Fizz"
+    if numeros_usuario[contador] % 5 == 0:
+        palabra+="Buzz"
+    if palabra == "FizzBuzz":
+        palabra="Bazinga"
+    if not palabra == "":
+        numeros_usuario[contador]=palabra
+        palabra = ""
+print(numeros_usuario)
